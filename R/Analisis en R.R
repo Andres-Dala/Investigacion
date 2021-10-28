0b5d9d118d8c47a9aea6fd8eeb9391f446a357e5
Andres Dala <andresht6@hotmail.com>
2021-10-28 12:41:16 -0600

    .

--- R/Analisis en R.R
+++ R/Analisis en R.R
@@ -1,7 +1,19 @@
-library(bayesforecast)
-library(bayesplot)
+library(tidyverse)
 library(forecast)
-library(ggplot2)
-options(mc.cores = parallel::detectCores())
+datos1<-read.csv("C:/Users/Andres Dala/Downloads/GlobalLandTemperaturesByCountry.csv")
+datos2<-read.csv("C:/Users/Andres Dala/Downloads/GlobalTemperatures.csv")
 
-#primer analisis
+honduras<-datos1 %>%
+  filter(Country == "Honduras")
+
+honduras$dt<-as.Date(honduras$dt)
+
+honduras <- honduras %>%
+  filter(as.numeric(format(dt,"%Y"))>=1980)
+
+honduras <- honduras %>%
+  select(dt, AverageTemperature)
+
+serie1<-ts(honduras$AverageTemperature, frequency = 12, start = 1980)
+
+autoplot(object = serie1)
