6579ecb9020be97782f992a77c2906543682e433
Andres Dala <andresht6@hotmail.com>
2021-10-28 17:19:09 -0600

    .

--- R/Analisis en R.R
+++ R/Analisis en R.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(forecast)
 datos<-read.csv("https://raw.githubusercontent.com/Andres-Dala/Seminario_de_Investigacion_MM700/main/R/LandTemperature.csv")
+# Origen de los datos : 
+#  http://berkeleyearth.org/data/
+#  https://data.world/data-society/global-climate-change-data/workspace/project-summary?agentid=data-society&datasetid=global-climate-change-data
 honduras<-datos %>%
   filter(Country == "Honduras")
 
@@ -14,4 +17,4 @@ honduras <- honduras %>%
 
 serie1<-ts(honduras$AverageTemperature, frequency = 12, start = 1980)
 
-autoplot(object = serie1, main = "Temperatura promedio en Honduras")
+autoplot(object = serie1, main = "Temperatura promedio en Honduras", ylab = "Celsius", xlab = "Años")
